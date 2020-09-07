d55db1ac058c1ced6211c8d42175378dfc514276
k-murakami0609 <dadgac@gmail.com>
2020-09-07 13:20:35 +0900

    ヘッダーのパースした

--- chapter2/src/elf.rs
+++ chapter2/src/elf.rs
@@ -50,3 +50,29 @@ impl ElfIdentification {
         }
     }
 }
+
+/// File identification in elf header.
+#[repr(packed)]
+pub struct ElfHeader {
+    pub e_type: u16,
+    pub e_machine: u16,
+    pub e_version: u32,
+    pub e_entry: u64,
+    pub e_phoff: u64,
+    pub e_shoff: u64,
+    pub e_flags: u32,
+    pub e_ehsize: u16,
+    pub e_phentsize: u16,
+    pub e_phnum: u16,
+    pub e_shentsize: u16,
+    pub e_shnum: u16,
+    pub e_shstrndx: u16,
+}
+
+impl ElfHeader {
+    pub fn new(binary: &[u8; 48]) -> ElfHeader {
+        return unsafe { std::mem::transmute::<[u8; 48], ElfHeader>(*binary) };
+    }
+}
+
+pub const ELF64_ADDR_SIZE: usize = std::mem::size_of::<ElfIdentification>();
--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -10,9 +10,33 @@ fn main() {
         return;
     }
     let identification = elf::ElfIdentification::new(&loader.mapped_file);
+    let mut binary: [u8; 48] = [0; 48];
+    for (i, b) in loader.mapped_file[elf::ELF64_ADDR_SIZE..48 + elf::ELF64_ADDR_SIZE]
+        .iter()
+        .enumerate()
+    {
+        binary[i] = *b;
+    }
+    let header = elf::ElfHeader::new(&binary);
     println!("Class   = {}", identification.class);
     println!("Data    = {}", identification.endianess);
     println!("Version = {}", identification.version);
     println!("OS/ABI = {}", identification.os_abi);
     println!("ABI Version = {}", identification.os_abi_version);
+
+    unsafe {
+        println!("Type = {}", header.e_type);
+        println!("Machine = {}", header.e_machine);
+        println!("Version = {}", header.e_version);
+        println!("Entry = {}", header.e_entry);
+        println!("P Offset = {}", header.e_phoff);
+        println!("S Offset = {}", header.e_shoff);
+        println!("Flags = {}", header.e_flags);
+        println!("Entry Size = {}", header.e_ehsize);
+        println!("P Size = {}", header.e_phentsize);
+        println!("P Number = {}", header.e_phnum);
+        println!("S Size = {}", header.e_shentsize);
+        println!("S Number = {}", header.e_shnum);
+        println!("Index = {}", header.e_shstrndx);
+    }
 }
